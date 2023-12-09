deaa4877970deb357466e22489a9d82e5536b803
qqsh1ne <qqsh1neee@gmail.com>
2023-12-09 13:50:21 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import Header from './components/Header/ui/Header.tsx';
 import Calendar from './components/Calendar/ui/Calendar.tsx';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './components/Login/ui/Login.tsx';
 import './App.css';
 import Table from './components/DataGridTable/ui/Table.tsx';
