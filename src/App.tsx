3d1ca97e30ebc0def7fcb2d980ec2e923d09e7a3
qqsh1ne <qqsh1neee@gmail.com>
2023-12-09 13:51:29 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import './App.css';
 import Header from './components/Header/ui/Header.tsx';
 import Calendar from './components/Calendar/ui/Calendar.tsx';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Login from './components/Login/ui/Login.tsx';
 import './App.css';
 import Table from './components/DataGridTable/ui/Table.tsx';
 
 function App() {
-	return (
-		<BrowserRouter>
-			<div className='app-wrapper'>
-				<Header />
-				<div className='app-wrapper-content'>
-					<Routes>
-						<Route path='/login' element={<Login />} />
-						<Route path='/calendar' element={<Calendar />} />
-					</Routes>
-				</div>
-			</div>
-		</BrowserRouter>
-	);
+    return (
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <div className='app-wrapper-content'>
+                    <Routes>
+                        <Route path='/login' element={<Login/>}/>
+                        <Route path='/calendar' element={<Calendar/>}/>
+                    </Routes>
+                </div>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
