e3670b9772185f737fe942144ecc36ab537461b8
qqsh1ne <qqsh1neee@gmail.com>
2023-12-09 13:49:35 +0500

    conf

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import './App.css';
-// import Header from './components/Header/ui/Header.tsx'
-// import Login from './components/Login/ui/Login.tsx';
 import Header from './components/Header/ui/Header.tsx';
 import Calendar from './components/Calendar/ui/Calendar.tsx';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
