5403388b9cd2e5183edf0d2cba54f18bae018024
qqsh1ne <qqsh1neee@gmail.com>
2023-12-09 13:51:49 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import Header from './components/Header/ui/Header.tsx';
 import Calendar from './components/Calendar/ui/Calendar.tsx';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Login from './components/Login/ui/Login.tsx';
 import './App.css';
 import Table from './components/DataGridTable/ui/Table.tsx';
@@ -10,7 +10,7 @@ function App() {
     return (
         <BrowserRouter>
             <div className='app-wrapper'>
-                <Header />
+                <Header/>
                 <div className='app-wrapper-content'>
                     <Routes>
                         <Route path='/login' element={<Login/>}/>
