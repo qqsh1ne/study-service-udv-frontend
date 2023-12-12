aa3c0e8113d198671c32d048e642956a3e8a0acc
Roman <romanlabzin03@gmail.com>
2023-12-12 15:05:48 +0500

    nikita chto eto za changes in dev??

--- src/components/DataGridTable/TableMock.ts
+++ src/components/DataGridTable/TableMock.ts
@@ -1,63 +1,62 @@
 import dayjs from 'dayjs';
 
 const TYPE_CONFERENCE = {
-    id: 1,
-    name: 'Конференция',
+	id: 1,
+	name: 'Конференция'
 };
 const TYPE_STUDY = {
-    id: 2,
-    name: 'Обучение',
+	id: 2,
+	name: 'Обучение'
 };
 const TYPE_EXAM = {
-    id: 3,
-    name: 'Экзамен',
+	id: 3,
+	name: 'Экзамен'
 };
 const TYPES = [TYPE_CONFERENCE, TYPE_STUDY, TYPE_EXAM];
 
 const STUDY_SOFT = {
-    id: 1,
-    name: 'Софты',
+	id: 1,
+	name: 'Софты'
 };
 const STUDY_HARD = {
-    id: 2,
-    name: 'Харды',
+	id: 2,
+	name: 'Харды'
 };
 const STUDY_MANAGING = {
-    id: 3,
-    name: 'Управление',
+	id: 3,
+	name: 'Управление'
 };
 const STUDIES = [STUDY_SOFT, STUDY_HARD, STUDY_MANAGING];
 
-
 export const rows = [
-    {
-        id: 1,
-        studyCentre: 'UrFU',
-        course: 'openedu',
-        type: TYPE_CONFERENCE,
-        member: 'Иванов Иван Иванович',
-        department: 'Крутой разработки',
-        team: 'Лушчая',
-        legalPerson: 'Иванов Иван Иванович',
-        startDate: dayjs('2023-09-10').toDate(),
-        endDate: dayjs('2023-12-12').toDate(),
-        price: 100000,
-        direction: STUDY_MANAGING,
-    },
-    {
-        id: 2,
-        studyCentre: 'UDV',
-        course: 'coursera',
-        type: TYPE_STUDY,
-        member: 'Сидоров Иван Васильевич',
-        department: 'Аналитики',
-        team: 'Крутая',
-        legalPerson: 'Крутой Игорь Николаев',
-        startDate: dayjs('2023-10-13').toDate(),
-        endDate: dayjs('2024-05-25').toDate(),
-        price: 10000000,
-        direction: STUDY_HARD,
-    },
+	{
+		id: 1,
+		studyCentre: 'UrFU',
+		course: 'openedu',
+		type: TYPE_CONFERENCE,
+		member: 'Иванов Иван Иванович',
+		department: 'Крутой разработки',
+		team: 'Лушчая',
+		legalPerson: 'Иванов Иван Иванович',
+		startDate: dayjs('2023-09-10').toDate(),
+		endDate: dayjs('2023-12-12').toDate(),
+		price: 100000,
+		direction: STUDY_MANAGING
+	},
+	{
+		id: 2,
+		studyCentre: 'UDV',
+		course: 'coursera',
+		type: TYPE_STUDY,
+		member: 'Сидоров Иван Васильевич',
+		department: 'Аналитики',
+		team: 'Крутая',
+		legalPerson: 'Крутой Игорь Николаев',
+		startDate: dayjs('2023-10-13').toDate(),
+		endDate: dayjs('2024-05-25').toDate(),
+		price: 10000000,
+		direction: STUDY_HARD
+	}
 ];
 
-export {STUDIES, TYPES};
+export { STUDIES, TYPES };
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,12 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
-const rootReducer = combineReducers({})
+const rootReducer = combineReducers({});
 
 export const setupStore = () => {
 	return configureStore({
 		reducer: rootReducer
-	})
-}
+	});
+};
 
-export type RootState = ReturnType<typeof rootReducer>
-export type AppStore = ReturnType<typeof setupStore>
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
